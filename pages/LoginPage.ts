da915e11a02590b6d1227332feb67db5cb065a5d
ahmed <ahmedzwawi46@gmail.com>
2025-03-19 10:45:39 +0100

    commit1

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -2,53 +2,51 @@ import { Page } from 'playwright';
 
 export class LoginPage {
   private page: Page;
+  private baseUrl: string;  // Ajout de baseUrl pour stocker l'URL
 
-  constructor(page: Page) {
+  constructor(page: Page, baseUrl: string) {  // Modification du constructeur pour accepter baseUrl
     this.page = page;
+    this.baseUrl = baseUrl;
   }
 
+  // Mettre à jour la méthode goto pour utiliser baseUrl
   async goto() {
-    await this.page.goto('http://192.168.1.95:9091/admin/login/?next=/admin/');
+    await this.page.goto(this.baseUrl);  // Utiliser baseUrl ici
   }
 
-  // async login(username: string, password: string) {
-  //   await this.page.fill('input[name="username"]', username);
-  //   await this.page.fill('input[name="password"]', password);
-  //   await this.page.click('input[type="submit"]');
-
-  // }
+  // Reste des éléments et méthodes...
   Elements = {
     username: 'input[name="username"]',
     password: 'input[name="password"]',
     submit: 'input[type="submit"]',
     error: '.errornote',
     dashboard: '.colMS'
-
   }
+
   async username(username: string) {
     await this.page.fill(this.Elements.username, username);
   }
+
   async password(password: string) {
     await this.page.fill(this.Elements.password, password);
   }
+
   async submit() {
     await this.page.click(this.Elements.submit);
   }
+
   async error() {
     return await this.page.textContent(this.Elements.error);
   }
+
   async isDashboardVisible() {
     return await this.page.isVisible(this.Elements.dashboard);
   }
 
-  // async isDashboardVisible(): Promise<boolean> {
-  //   return await this.page.isVisible('.colMS');
-//}
- // Méthode de login combinée
- async login(username: string, password: string) {
-  await this.username(username);
-  await this.password(password);
-  await this.submit();
-}
-
+  // Méthode de login combinée
+  async login(username: string, password: string) {
+    await this.username(username);
+    await this.password(password);
+    await this.submit();
+  }
 }
